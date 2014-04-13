87aee7d2ed70ed7bbf9623c8928bec4d5ee96b67
José Pinto <zepinto@gmail.com>
2014-04-13 11:19:29 +0100

    Moved Argos web service code to jar.

--- app/fr/cls/argos/dataxmldistribution/service/types/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.dataxmldistribution.argos.cls.fr/types", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package fr.cls.argos.dataxmldistribution.service.types;
