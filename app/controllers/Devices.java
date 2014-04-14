8b38d8eb39741a933849e22b52354f181f288a63
José Pinto <zepinto@gmail.com>
2014-04-14 15:21:08 +0100

    Work in progress.

--- app/controllers/Devices.java
+++ app/controllers/Devices.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import models.Device;
 import play.data.Form;
@@ -16,8 +17,22 @@ public class Devices extends Controller {
         return ok(views.html.devlist.render(Device.find.all()));
     }
 	
-	public static Result index() {
+	public static Result active() {
 		
+		ArrayList<Device> devices = new ArrayList<Device>();
+		ArrayList<models.System> systems = new ArrayList<models.System>();
+		Date oneDayAgo = new Date(System.currentTimeMillis() - 24 * 3600 * 1000);
+		
+		devices.addAll(Device.find.all());
+		for (Device dev : devices) {
+			if (dev.position != null && dev.position != null && dev.position.timestamp.after(oneDayAgo))
+			systems.add(new models.System(dev));
+		}
+		
+        return ok(Json.toJson(systems));
+    }
+	
+	public static Result index() {
 		ArrayList<Device> devices = new ArrayList<Device>();
 		ArrayList<models.System> systems = new ArrayList<models.System>();
 		
@@ -31,20 +46,46 @@ public class Devices extends Controller {
 	
 	
 	public static Result create() {
-		return ok(views.html.device.render(frm, null));		
+		Form<Device> deviceForm = Form.form(Device.class);
+		return ok(views.html.editDevice.render(deviceForm, true, 0l));
 	}
 	
 	public static Result submit() {
-		return TODO;
+		try {
+			System.out.println(request().body().asMultipartFormData());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			System.out.println(request().body().asJson());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return created();
 	}
 	
 	public static Result get(long id) {
 		Device dev = Device.find.byId(id);
 		if (dev == null)
-			return notFound("ID not found.");
+			return notFound("404,Device not found,"+id);
 		else {
+			Form<Device> deviceForm = Form.form(Device.class);
 			frm.fill(dev);
-			return ok(views.html.device.render(frm, dev));
-		}	
+			return ok(views.html.editDevice.render(deviceForm, false, id));
+		}
+	}
+	
+	public static Result delete(long id) {
+		Device dev = Device.find.byId(id);
+		if (dev == null)
+			return badRequest("500,Device not found,"+id);
+		else {
+			dev.delete();
+			return ok("200,Device has been deleted,"+id);
+		}
 	}
 }
